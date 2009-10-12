3a8b12b38d802d72449eaf8c5b934fb9b6392ddd
Alan Gutierrez <alan@blogometer.com>
2009-10-11 23:03:18 -0500

    Renamed CassandraProject to CassandraModule.

--- src/mix/java/com/goodworkalan/cassandra/mix/CassandraModule.java
+++ src/mix/java/com/goodworkalan/cassandra/mix/CassandraModule.java
@@ -3,9 +3,9 @@ package com.goodworkalan.cassandra.mix;
 import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.BasicJavaModule;
 
-public class CassandraProject extends BasicJavaModule {
-    public CassandraProject() {
-        super(new Artifact("com.goodworkalan", "cassandra", "0.7-SNAPSHOT"));
+public class CassandraModule extends BasicJavaModule {
+    public CassandraModule() {
+        super(new Artifact("com.goodworkalan", "cassandra", "0.7"));
         addTestDependency(new Artifact("org.testng", "testng", "5.10"));
     }
 }
