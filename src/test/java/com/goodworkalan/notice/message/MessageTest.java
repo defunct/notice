7b78b4b884ae6c8d740d27ba71bed5e44b44135a
Alan Gutierrez <alan@blogometer.com>
2010-02-03 00:04:22 -0600

    Testing all valid paths.

--- src/test/java/com/goodworkalan/notice/message/MessageTest.java
+++ src/test/java/com/goodworkalan/notice/message/MessageTest.java
@@ -1,6 +1,7 @@
 package com.goodworkalan.notice.message;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -20,6 +21,16 @@ public class MessageTest {
     private Message makeMessage(Object variables) {
         return new Message(new ConcurrentHashMap<String, ResourceBundle>(), MessageTest.class.getCanonicalName(), "test_messages", "key", variables);
     }
+    
+    private Message makePopulatedMessage() {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        HashMap<String, Object> subMap = new HashMap<String, Object>();
+        map.put("a", "b");
+        map.put("b", subMap);
+        subMap.put("c", "d");
+        subMap.put("e", Arrays.asList("a", "b", "c"));
+        return makeMessage(map);
+    }
 
     /**
      * Check the attribute accessors.
@@ -36,19 +47,59 @@ public class MessageTest {
     }
     
     /**
-     * Check the attribute accessors.
+     * Check a single map dereference.
      */
     @Test
-    public void paths() {
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        HashMap<String, Object> subMap = new HashMap<String, Object>();
-        map.put("a", "b");
-        map.put("b", subMap);
-        subMap.put("c", "d");
-        subMap.put("e", Arrays.asList("a", "b", "c"));
-        Message message = makeMessage(map);
-        assertEquals(message.get("a"), "b");
-        assertEquals(message.get("b.c"), "d");
-        assertEquals(message.get("b.e.1"), "b");
+    public void mapGet() {
+        assertEquals(makePopulatedMessage().get("a"), "b");
+    }
+   
+    /**
+     * Check getting a value from a nested map.
+     */
+    @Test
+    public void nestedMapGet() {
+        assertEquals(makePopulatedMessage().get("b.c"), "d");
+    }
+    
+    /**
+     * Check getting a list value.
+     */
+    @Test
+    public void listGet() {
+        assertEquals(makePopulatedMessage().get("b.e.1"), "b");
+    }
+    
+    /**
+     * Check referencing a null hash element.
+     */
+    @Test
+    public void noSuchKey() {
+        assertNull(makePopulatedMessage().get("z"));
+    }
+    
+    /**
+     * Check a missing hash element.
+     */
+    @Test
+    public void noSuchHashElement() {
+        assertNull(makePopulatedMessage().get("b.c.d"));
+    }
+    
+    /**
+     * Check an out of range array element.
+     */
+    @Test
+    public void noSuchListElement() {
+        assertNull(makePopulatedMessage().get("b.e.8"));
+        assertNull(makePopulatedMessage().get("b.e.3"));
+    }
+
+    /**
+     * Check referencing an array with a valid Java identifier.
+     */
+    @Test
+    public void noSuchStringElementInList() {
+        assertNull(makePopulatedMessage().get("b.e.f"));
     }
 }
