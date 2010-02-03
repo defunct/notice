9c62bfa2932f6a01e9c4762d945c763361b14ac3
Alan Gutierrez <alan@blogometer.com>
2010-02-02 23:39:38 -0600

    Created test for Message basic accessors.

--- src/test/java/com/goodworkalan/notice/message/MessageTest.java
+++ src/test/java/com/goodworkalan/notice/message/MessageTest.java
@@ -1,7 +1,13 @@
 package com.goodworkalan.notice.message;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.testng.annotations.Test;
 
@@ -11,6 +17,25 @@ import org.testng.annotations.Test;
  * @author Alan Gutierrez
  */
 public class MessageTest {
+    /** Create a message with the given variables. */
+    private Message makeMessage(Object variables) {
+        return new Message(new ConcurrentHashMap<String, ResourceBundle>(), MessageTest.class.getCanonicalName(), "test_messages", "key", variables);
+    }
+
+    /**
+     * Check the attribute accessors.
+     */
+    @Test
+    public void accessors() {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("a", "b");
+        Message message = makeMessage(map);
+        assertEquals(message.getBundleName(), "test_messages");
+        assertEquals(message.getMessageKey(), "key");
+        assertEquals(message.getContext(), "com.goodworkalan.notice.message.MessageTest");
+        assertEquals(((Map<?, ?>) message.getVariables()).get("a"), "b");
+    }
+    
     /**
      * Check a valid Java identifier.
      */
