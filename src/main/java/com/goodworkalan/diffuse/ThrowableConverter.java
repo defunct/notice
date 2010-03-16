0a5fa7d6526ea4d7281d09ceafde27fe329960d6
Alan Gutierrez <alan@blogometer.com>
2010-03-15 23:41:02 -0500

    Extracted Diffuse and refactored for Verbiage.
     * Extracted Diffuse to its own project.
     * Updated project dependencies for Notice-Message to Verbiage project name
       refactor.
    
    [#22 state:resolved] [#23]

--- src/main/java/com/goodworkalan/diffuse/ThrowableConverter.java
+++
@@ -1,9 +0,0 @@
-package com.goodworkalan.diffuse;
-
-
-
-public class ThrowableConverter extends BeanConverter {
-    public boolean isContainer() {
-        return false;
-    }
-}
--- src/main/java/com/goodworkalan/notice/Notice.java
+++ src/main/java/com/goodworkalan/notice/Notice.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import com.goodworkalan.diffuse.Diffuse;
+import com.goodworkalan.diffuse.MapConverter;
 import com.goodworkalan.notice.message.Message;
 
 /**
