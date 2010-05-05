69a18d9976b4a3d85cb4f090b4e4f1ebd7ed3b9f
Alan Gutierrez <alan@blogometer.com>
2010-05-05 01:55:48 -0500

    Updated Mix build for new project language.

--- src/mix/java/com/goodworkalan/notice/json/mix/NoticeJsonProject.java
+++ src/mix/java/com/goodworkalan/notice/json/mix/NoticeJsonProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.notice.json.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,19 +9,19 @@ public class NoticeJsonProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/notice-json/0.1"))
+                .produces("com.github.bigeasy.notice/notice-json/0.1")
                 .main()
                     .depends()
-                        .artifact(new Artifact("com.goodworkalan/notice-rotate/0.1"))
-                        .artifact(new Artifact("com.googlecode.json-simple/json-simple/1.1"))
+                        .include("com.github.bigeasy.notice/notice-rotate/0.+1")
+                        .include("com.googlecode.json-simple/json-simple/1.1")
                         .end()
                     .end()
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.slf4j/slf4j-log4j12/1.4.2"))
-                        .artifact(new Artifact("log4j/log4j/1.2.14"))
-                        .artifact(new Artifact("org.testng/testng/5.10"))
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
+                        .include("org.slf4j/slf4j-log4j12/1.4.2")
+                        .include("log4j/log4j/1.2.14")
+                        .include("org.testng/testng-jdk15/5.10")
+                        .include("org.mockito/mockito-core/1.6")
                         .end()
                     .end()
                 .end()
