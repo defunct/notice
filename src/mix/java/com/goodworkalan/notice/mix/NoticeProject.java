94e8071cf0137854c44bddae5efe3329c04d2315
Alan Gutierrez <alan@blogometer.com>
2010-05-16 23:28:09 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/notice/mix/NoticeProject.java
+++ src/mix/java/com/goodworkalan/notice/mix/NoticeProject.java
@@ -4,29 +4,33 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class NoticeProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Spawn.
+ *
+ * @author Alan Gutierrez
+ */
+public class NoticeProject implements ProjectModule {
+    /**
+     * Build the project definition for Spawn.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.notice/notice/0.1.0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.retry/retry/0.+1")
-                        .include("com.github.bigeasy.verbiage/verbiage/0.+1")
-                        .include("com.github.bigeasy.diffuse/diffuse/0.+1")
-                        .include("com.github.bigeasy.reflective/reflective/0.+1")
-                        .include("com.github.bigeasy.madlib/madlib/0.+1")
-                        .include("org.slf4j/slf4j-api/1.4.2")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.slf4j/slf4j-log4j12/1.4.2")
-                        .include("log4j/log4j/1.2.14")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.retry/retry/0.+1")
+                    .production("com.github.bigeasy.verbiage/verbiage/0.+1")
+                    .production("com.github.bigeasy.diffuse/diffuse/0.+1")
+                    .production("com.github.bigeasy.reflective/reflective/0.+1")
+                    .production("com.github.bigeasy.madlib/madlib/0.+1")
+                    .production("org.slf4j/slf4j-api/1.4.2")
+                    .development("org.slf4j/slf4j-log4j12/1.4.2")
+                    .development("log4j/log4j/1.2.14")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
