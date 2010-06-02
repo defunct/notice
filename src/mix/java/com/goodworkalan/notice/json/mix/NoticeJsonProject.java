7c42394f52464c4dad6919897d64c4cddea2bcbb
Alan Gutierrez <alan@blogometer.com>
2010-06-01 21:42:29 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/notice/json/mix/NoticeJsonProject.java
+++ src/mix/java/com/goodworkalan/notice/json/mix/NoticeJsonProject.java
@@ -4,25 +4,29 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class NoticeJsonProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Notice JSON.
+ *
+ * @author Alan Gutierrez
+ */
+public class NoticeJsonProject implements ProjectModule {
+    /**
+     * Build the project definition for Notice JSON.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.notice/notice-json/0.1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.notice/notice-rotate/0.+1")
-                        .include("com.googlecode.json-simple/json-simple/1.1")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.slf4j/slf4j-log4j12/1.4.2")
-                        .include("log4j/log4j/1.2.14")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .include("org.mockito/mockito-core/1.6")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.notice/notice-rotate/0.+1")
+                    .production("com.googlecode.json-simple/json-simple/1.1")
+                    .development("org.slf4j/slf4j-log4j12/1.4.2")
+                    .development("log4j/log4j/1.2.14")
+                    .development("org.testng/testng-jdk15/5.10")
+                    .development("org.mockito/mockito-core/1.6")
                     .end()
                 .end()
             .end();
