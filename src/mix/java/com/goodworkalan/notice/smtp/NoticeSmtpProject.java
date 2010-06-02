d981682d28c9ffa01936d940bdc4918f3660e87a
Alan Gutierrez <alan@blogometer.com>
2010-06-01 21:56:23 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/notice/smtp/NoticeSmtpProject.java
+++ src/mix/java/com/goodworkalan/notice/smtp/NoticeSmtpProject.java
@@ -4,22 +4,26 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class NoticeSmtpProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Notice SMTP.
+ *
+ * @author Alan Gutierrez
+ */
+public class NoticeSmtpProject implements ProjectModule {
+    /**
+     * Build the project definition for Notice SMTP.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.notice/notice-smtp/0.+1")
-                .main()
-                    .depends()
-                        .include("com.github.bigeasy.notice/notice/0.+1")
-                        .include("com.github.bigeasy.waste/waste/0.+7")
-                        .end()
-                    .end()
-                .test()
-                    .depends()
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .production("com.github.bigeasy.notice/notice/0.+1")
+                    .production("com.github.bigeasy.waste/waste/0.+7")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
